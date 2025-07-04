54e7dccdd4e4adc86ccf58bf45e0fb9501f79ad7
Pablo Gabriel Mederos <pablo.mederos@lacuevadev.cloud>
2025-07-04 17:13:16 +0000

    docs: delete fixgithublinks.js

--- fixgithublinks.js
+++
@@ -1,5 +0,0 @@
-Array.from(document.querySelectorAll('.page-edit-shortcuts.is-right a'))
-    .filter(link => link.href.includes('github.com'))
-    .forEach(link => {
-        link.href = link.href.replace('/es/', '/');
-    });
